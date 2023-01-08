49b302d452699ae5608545c2d30c8c3a915cd758
unknown <artem4rolov@gmail.com>
2023-01-08 23:45:53 +0300

    делаем шапку

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import styles from "../styles";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return <div>GetStarted</div>;
-};
-
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-gradient p-[2px] cursor-pointer`}
+  >
+    GetStarted
+  </div>
+);
 export default GetStarted;
